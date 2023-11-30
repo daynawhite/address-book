cea603bd65b6a52ed92fb73daa3a6e0be048f6ee
daynawhite <dayna26.2@gmail.com>
2023-11-29 19:06:17 -0600

    cleaned up the code a bit, mostly by deleting or adding comments

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-// Use the API: https://randomuser.me/api/ to fetch a user now
-// Fetch a new user multiple times and store them in an array
-// Then list out all the users in your address book array by name and picture
-// Figure out how to fetch multiple users in one fetch request
-// Fetch multiple users on window load
-// Add a button to each user that when clicked displays the rest of their information like DOB, address and so forth
-// Once you have the functionality working, feel free to style and structure your address book with CSS and HTML
 
-
-       // function that will reveal additional information from the API
-    //    const buttonFunction = () => {
-        // if (div.style.display === "none") {
-        //     div.style.display = "block";
-        // } else {
-        //     div.style.display = "none"
-        // }
-    // }
-    
 const displayPost = () => {
-  
     const allPosts = document.getElementById('all-posts')
     posts.map((post) => {
       const li = document.createElement('li');
@@ -30,11 +12,10 @@ const displayPost = () => {
       text.innerHTML = `Name: ${post.name.first} ${post.name.last}`;
       li.appendChild(text);
       allPosts.append(li);
-    //   add a button that will reveal additional information when clicked:
+    // create a button that will reveal additional information when clicked:
       const button = document.createElement('button');
       button.innerHTML = 'More';
-    //   button.onclick = "buttonFunction()";
-
+    // create div element to hold additional information that will appear when button is clicked:
     const div = document.createElement('div');
     const email = document.createElement('p');
     email.innerHTML = `Email: ${post.email}`;
@@ -43,14 +24,9 @@ const displayPost = () => {
     div.appendChild(email);
     div.appendChild(cell);
     li.appendChild(div)
-      
-      div.style.display = "none"
-
+    div.style.display = "none"
+    // function that will allow the button to make information appear and disappear.
     button.onclick = () => {
-        // li.append(div)
-        // allPosts.append(div)
-        // div.style.display = "block"}
-      
       if (div.style.display === "none") {
         div.style.display = "block";
         button.innerHTML = 'Less'
@@ -58,16 +34,12 @@ const displayPost = () => {
         div.style.display = "none"
         button.innerHTML = 'More'
     }
-
-       
     }
-        // button.onclick = appear()
-        li.appendChild(button);
-
-    //   button.onclick = buttonFunction()
+    // add the button to the DOM:
+    li.appendChild(button);
 })
 }
-
+// API request and call function that organizes the returned info in the DOM
 window.onload = async function getPosts() {
     const response = await fetch('https://randomuser.me/api/?results=10');
     rawPosts = await response.json();
